7a9f4d8c9e0185f9c0b9669d2ce587255129e322
vkoralla <vashistareddy3@gmail.com>
2022-08-14 15:01:17 -0500

    update docusaurus.config.js

--- test_website_docs/docusaurus.config.js
+++ test_website_docs/docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/vrsky/test_docbuilder/tree/main/test_website_docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/vrsky/test_docbuilder/tree/main/test_website_docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
